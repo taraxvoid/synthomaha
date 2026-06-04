f7a1312fa828f6148e68aadc1ec1e0c9caa6214f
void <1711810+taraxvoid@users.noreply.github.com>
2026-06-04 15:35:41 -0500

    lint

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-import react from '@astrojs/react'
-import tailwindcss from '@tailwindcss/vite'
-import { defineConfig } from 'astro/config'
+import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  },
-  integrations: [react()],
-})
+    vite: {
+        plugins: [tailwindcss()]
+    },
+    integrations: [react()]
+});
